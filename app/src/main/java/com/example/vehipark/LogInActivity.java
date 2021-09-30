33db1d9c6a5251195e83c00cd094c00a37f88cec
bihansa <88286620+bihansa@users.noreply.github.com>
2021-09-30 13:44:25 +0530

    authentication comited

--- app/src/main/java/com/example/vehipark/LogInActivity.java
+++ app/src/main/java/com/example/vehipark/LogInActivity.java
@@ -1,40 +1,74 @@
 package com.example.vehipark;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class LogInActivity extends AppCompatActivity {
+    TextView loginEmail;
+    TextView loginPassword;
+    TextView loginBack;
+    TextView clickHere;
+    Button loginButton;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextView btn=findViewById(R.id.textViewSignUp);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LogInActivity.this,SignUpActivity.class));
-            }
-        });
-        TextView btn1=findViewById(R.id.textViewFpass);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LogInActivity.this,ForgotPasswordActivity.class));
-            }
-        });
 
-        Button btn2=findViewById(R.id.button);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LogInActivity.this,HomeActivity.class));
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPassword = findViewById(R.id.loginPassword);
+        loginBack = findViewById(R.id.loginBack);
+        clickHere = findViewById(R.id.clickHere);
+        loginButton =findViewById(R.id.loginButton);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(view ->{
+            String email = loginEmail.getText().toString();
+            String password = loginPassword.getText().toString();
+
+            if (TextUtils.isEmpty(email)){
+                loginEmail.setError("Email cannot be empty");
+                loginEmail.requestFocus();
+            }else if(TextUtils.isEmpty(password)){
+                loginPassword.setError("Password cannot be empty");
+                loginPassword.requestFocus();
+            }else{
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LogInActivity.this,"Sucessful", Toast.LENGTH_SHORT).show();
+                            startActivity( new Intent(LogInActivity.this, HomePageActivity.class));
+                        }else{
+                            Toast.makeText(LogInActivity.this, "Error",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
+        });
+        loginBack.setOnClickListener(view ->{
+            startActivity(new Intent(LogInActivity.this, SignUpActivity.class));
+        });
+        clickHere.setOnClickListener(view ->{
+            startActivity(new Intent(LogInActivity.this, ForgotPasswordActivity.class));
         });
     }
+
 }
--- app/src/main/java/com/example/vehipark/SignUpActivity.java
+++ app/src/main/java/com/example/vehipark/SignUpActivity.java
@@ -1,25 +1,105 @@
 package com.example.vehipark;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText regEmail;
+    EditText regPassword;
+    EditText regConfirmPassword;
+    TextView regBackLogin;
+    TextView regName;
+    TextView regContact;
+    TextView regAddress;
+    Button regButton;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        TextView btn=findViewById(R.id.textViewLogIn);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        regEmail = findViewById(R.id.regEmail);
+        regPassword = findViewById(R.id.regPassword);
+        regConfirmPassword = findViewById(R.id.regConfirmPassword);
+        regBackLogin = findViewById(R.id.regBackLogin);
+        regButton = findViewById(R.id.signUpButton);
+        regName = findViewById(R.id.regName);
+        regContact = findViewById(R.id.regContact);
+        regAddress = findViewById(R.id.regAddress);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        regButton.setOnClickListener(view ->{
+            createUser();
+        });
+        regBackLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(SignUpActivity.this,LogInActivity.class));
             }
         });
-
     }
+    private void createUser() {
+        String email = regEmail.getText().toString();
+        String password = regPassword.getText().toString();
+        String confirmPassword = regConfirmPassword.getText().toString();
+        String name = regName.getText().toString();
+        String contact = regContact.getText().toString();
+        String address = regAddress.getText().toString();
+
+        if(TextUtils.isEmpty(name)){
+            regName.setError("Name cannot be empty");
+            regName.requestFocus();
+        }
+        if(TextUtils.isEmpty(address)) {
+            regEmail.setError("address cannot be Empty");
+            regEmail.requestFocus();
+        }
+        if(TextUtils.isEmpty(contact)) {
+            regEmail.setError("contact cannot be Empty");
+            regEmail.requestFocus();
+        }
+        if(TextUtils.isEmpty(email)) {
+            regEmail.setError("Email cannot be Empty");
+            regEmail.requestFocus();
+        }
+        if (TextUtils.isEmpty(password)) {
+            regPassword.setError("Password cannot be Empty");
+            regPassword.requestFocus();
+        }
+        if (!confirmPassword.equals(password)) {
+            regConfirmPassword.setError("Passwords does not Match");
+            regConfirmPassword.requestFocus();
+        }
+        else{
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(SignUpActivity.this, "Sucessful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(SignUpActivity.this, HomePageActivity.class));
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
 }
