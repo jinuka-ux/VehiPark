f25e018df1270f415a67a5b45425cf1774717abd
Jinuka <jinukaherath123.jh@gmail.com>
2021-10-26 23:51:44 +0530

    helooo

--- app/src/main/java/com/example/vehipark/SlotSelectionActivity.java
+++ app/src/main/java/com/example/vehipark/SlotSelectionActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
